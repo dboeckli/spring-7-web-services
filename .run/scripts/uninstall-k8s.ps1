e0e055972452bcab7df608db93d23b620198d688
Dominique Boeckli <dboeckli@gmail.com>
2026-01-24 18:16:05 +0100

    implement webservice 3

--- .run/scripts/uninstall-k8s.ps1
+++ .run/scripts/uninstall-k8s.ps1
@@ -1,6 +1,6 @@
 cd target/helm/repo
 
-$file = Get-ChildItem -Filter spring-7-project-template-v*.tgz | Select-Object -First 1
+$file = Get-ChildItem -Filter spring-7-web-services-v*.tgz | Select-Object -First 1
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
 
-helm uninstall $APPLICATION_NAME --namespace spring-7-project-template
+helm uninstall $APPLICATION_NAME --namespace spring-7-web-services
