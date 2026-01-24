3c0b756a17ca6a62b43621c18b85d7bbe1baf787
Dominique Boeckli <dboeckli@gmail.com>
2026-01-24 19:49:14 +0100

    add unit test for rest

---
+++ src/main/java/ch/dboeckli/soap/service/producingwebservice/CountryRestController.java
@@ -0,0 +1,29 @@
+package ch.dboeckli.soap.service.producingwebservice;
+
+import ch.dboeckli.soap.service.producingwebservice.schema.Country;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/countries")
+public class CountryRestController {
+
+    private final CountryRepository countryRepository;
+
+    public CountryRestController(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @GetMapping(value = "/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Country> getCountry(@PathVariable String name) {
+        Country country = countryRepository.findCountry(name);
+        if (country == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(country);
+    }
+}
